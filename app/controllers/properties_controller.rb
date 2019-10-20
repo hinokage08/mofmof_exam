65e571466854c391b59a3355e85f8566169a4450
youichi yamazaki <denncoru@yahoo.co.jp>
2019-10-21 05:18:17 +0900

    物件一覧画面を作成(index.html.erb)

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,19 @@
 class PropertiesController < ApplicationController
   def index
+    @properties = Property.all
   end
 
   def new
     @property = Property.new
+  end
+
+  def create
+    Property.create(property_params)
+    redirect_to new_property_path
+  end
+
+  private
+  def property_params
+    params.require(:property).permit(:property_name, :address, :rent, :age, :remarks)
   end
 end
