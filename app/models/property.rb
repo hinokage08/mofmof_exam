658f4dd12a7294a364ca098f1d02a3df2afa719c
youichi yamazaki <denncoru@yahoo.co.jp>
2019-10-23 11:29:40 +0900

    登録画面(new.html.erb)にバリデーションで弾かれた際にエラーを表示するよう追記

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
   has_many :nearest_stations
   accepts_nested_attributes_for :nearest_stations, allow_destroy: true
-  validates :roperty_name, presence:true, length:{ maximum: 30 }
+  validates :property_name, presence:true, length:{ maximum: 30 }
   validates :rent,  presence:true, length:{ maximum: 10 }
   validates :address,  presence:true, length:{ maximum: 40 }
   validates :age,  presence:true, length:{ maximum: 3 }
